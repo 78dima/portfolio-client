36aa4ddc1075e0d53ea2184865e79f06e57c51a7
dromanenko <dmitry.romanenko@mvideo.ru>
2023-03-16 20:41:46 +0300

    create project

--- modules/posts/selectors.ts
+++ modules/posts/selectors.ts
@@ -1,9 +1,9 @@
 import { createSelector } from 'reselect';
 
-import { PostAwareState, PostsLoadingState, PostState } from './contracts/state';
+import { PostAwareState, PostsLoadingState } from './contracts/state';
 import { POSTS_MODULE_NAME } from './index';
 
-const postsSelector = (state: PostAwareState): PostState => state[POSTS_MODULE_NAME];
+// const postsSelector = (state: PostAwareState): PostState => state[POSTS_MODULE_NAME];
 export const postsLoadingStateSelector = (state: PostAwareState): PostsLoadingState =>
   state[POSTS_MODULE_NAME].loadingState;
 
