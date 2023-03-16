720187054d61be0d91f07f3bda8efab0f20c327b
dromanenko <dmitry.romanenko@mvideo.ru>
2023-03-16 20:25:06 +0300

    create project

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Modal = ({ children }) => {
-  const [trigger, setTrigger] = useState();
-
   <div>
     <div>{children}</div>
   </div>;
