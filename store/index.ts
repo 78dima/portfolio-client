c53d49ebefdd8651ddf622cb7cc9684ebbc917f6
dromanenko <dmitry.romanenko@mvideo.ru>
2023-03-16 20:42:49 +0300

    create project

--- store/index.ts
+++ store/index.ts
@@ -1,12 +1,12 @@
 import { composeWithDevTools } from '@redux-devtools/extension';
-import { Context, createWrapper } from 'next-redux-wrapper';
+import { createWrapper } from 'next-redux-wrapper';
 import { AnyAction, applyMiddleware, legacy_createStore as createStore, Store } from 'redux';
 import thunk, { ThunkDispatch } from 'redux-thunk';
 
 import { reducer, RootState } from './reducers/index';
 
 // create a makeStore function
-const makeStore = (context: Context) => createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const makeStore = () => createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 // export an assembled wrapper
 export const wrapper = createWrapper<Store<RootState>>(makeStore, { debug: true });
