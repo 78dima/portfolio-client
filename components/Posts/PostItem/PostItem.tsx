1f208a8ba124b59ba7a95e8abca74f15c32cc3f4
dromanenko <7878dima7878@gmail.com>
2023-03-28 00:00:06 +0300

    create project

--- components/Posts/PostItem/PostItem.tsx
+++ components/Posts/PostItem/PostItem.tsx
@@ -11,27 +11,24 @@ interface Props {
   stacking: Array<string>;
 }
 
-const PostItem: React.FC<Props> = ({ title, text, site, years, stacking }) => {
-  const data = [];
-  return (
-    <div className={styles.postItem}>
-      <div className={styles.postDate}>{years}</div>
-      <div className={styles.postContentWrapper}>
-        <h4 className={styles.postContentTitle}>{title}</h4>
-        <div className={styles.postContentSite}>
-          <a href={site}>{site}</a>
-        </div>
-        <div className={styles.postContentText}>{text}</div>
-        <div className={styles.postSkills}>
-          {stacking.map((item) => (
-            <div key={uuid()} className={styles.postSkillsItem}>
-              {item}
-            </div>
-          ))}
-        </div>
+const PostItem: React.FC<Props> = ({ title, text, site, years, stacking }) => (
+  <div className={styles.postItem}>
+    <div className={styles.postDate}>{years}</div>
+    <div className={styles.postContentWrapper}>
+      <h4 className={styles.postContentTitle}>{title}</h4>
+      <div className={styles.postContentSite}>
+        <a href={site}>{site}</a>
+      </div>
+      <div className={styles.postContentText}>{text}</div>
+      <div className={styles.postSkills}>
+        {stacking.map((item) => (
+          <div key={uuid()} className={styles.postSkillsItem}>
+            {item}
+          </div>
+        ))}
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default PostItem;
